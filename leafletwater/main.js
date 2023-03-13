9f0364becb78e0430c510adcd86c28886e4a8a23
Goutam Konapala <goutam.iitbbs@gmail.com>
2023-03-13 15:42:39 -0700

    cleaned files

--- leafletwater/main.js
+++ leafletwater/main.js
@@ -1,23 +1,69 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+import 'leaflet/dist/leaflet.css'
+import 'leaflet-geosearch/dist/geosearch.css';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
 
-setupCounter(document.querySelector('#counter'))
+import L from 'leaflet'
+
+import { GeoSearchControl, OpenStreetMapProvider } from 'leaflet-geosearch';
+
+
+
+let provider = new OpenStreetMapProvider();
+
+const searchControl = new GeoSearchControl({
+  provider: provider,
+  autoComplete: true, // optional: true|false  - default true
+  autoCompleteDelay: 250,
+  style: 'bar',
+  params: {
+    countrycodes: 'us' // limit search results to Canada & United States
+  }
+});
+
+const map = L.map('map', {
+  center: [35.50, -90],
+  zoom: 5
+});
+map.addControl(searchControl);
+
+var southWest = L.latLng(30.396308, -180),
+  northEast = L.latLng(70.384358, -60.885444);
+var bounds = L.latLngBounds(southWest, northEast);
+
+L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.{ext}', {
+  attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  subdomains: 'abcd',
+  ext: 'png',
+  minNativeZoom: 4,
+  minZoom: 4,
+  tms: false,
+  bounds: bounds
+}).addTo(map);
+
+function getColor(d) {
+  return d > 200 ? '#b10026' :
+    d > 100 ? '#e31a1c' :
+      d > 50 ? '#fc4e2a' :
+        d > 40 ? '#fd8d3c' :
+          d > 20 ? '#feb24c' :
+            d > 10 ? '#ffeda0' :
+              d > 5 ? '#ffffcc' :
+                '#238b45';
+}
+
+L.geoPackageFeatureLayer([], {
+  geoPackageUrl: 'https://temmdata.s3.us-east-005.backblazeb2.com/violations.gpkg',
+  layerName: 'violations',
+  style: function(feature) {
+    return {
+      fillColor: getColor(feature.properties.violations),
+      weight: 0.2,
+      opacity: 1,
+      color: 'white',
+      dashArray: '3',
+      fillOpacity: 0.7
+    };
+  },
+
+}).addTo(map);
